22e9cf4d274d1cfceb2ba14a42f47f67cb4b4429
Oliver Vatne Johansen <otjohans@student.matnat.uio.no>
2020-06-24 11:16:49 +0200

    Finishing web scraper tutorial

--- ExampleScraper/scraper.py
+++ ExampleScraper/scraper.py
@@ -1,10 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
 
+
+#This entire code is based on the guide from the page: "https://www.dataquest.io/blog/web-scraping-tutorial-python/"
 def print_test():
     p = False
-    print("Printer is set to:", p)
+    print("Printer for function 'print_test' is set to:", p)
 
     page = requests.get("http://dataquestio.github.io/web-scraping-pages/simple.html")
     if p:
@@ -43,14 +46,85 @@ def print_test():
 
 
 def scraping_classes_and_ids():
-    p = True
+    p = False
+    print("Printer set for the function called 'scraping_classes_and_ids' is set to:", p, "\n")
+
+    #Setting up the page to be parsed
+    page = requests.get("http://dataquestio.github.io/web-scraping-pages/ids_and_classes.html")
+    soup = BeautifulSoup(page.content, "html.parser")
 
 
+    #Parsing the document using soup for the class called 'outer text'
+    tag = soup.find_all('p', class_="outer-text")
+    if p:
+        print(tag)
+        print("\nSearching for tags using just the id:") 
+
+        print(soup.find_all(id="first")[0].get_text(), "\n")
+
+
+    #Parsing through the document using CSS-selector:
+    if p:
+        ret = soup.select("div p")
+        print("Printing p tags inside of div tags:\n", ret)
+
+
+def scraping_main_page():
+    p = False
+    print("Printing for the main page is:", p)
+
+    page = requests.get("https://forecast.weather.gov/MapClick.php?lat=37.7772&lon=-122.4168")
+    if page.status_code != 200:
+        print("Please check the connection.. ")
+        exit()
+    
+
+    soup = BeautifulSoup(page.content, "html.parser")
+    seven_day = soup.find(id="seven-day-forecast")
+    
+    if p:
+        print("Seven day:\n", seven_day)
+    
+    forecast_items = seven_day.find_all(class_="tombstone-container")
+    today = forecast_items[0]
+    if p:
+        print("\n\nForecast container:\n", forecast_items)
+        print("\n\nPrinting the first forecast_item:", today.prettify())
+    
+
+    period_tags = seven_day.select(".tombstone-container .period-name")
+    periods = [i.get_text() for i in period_tags]
+    descs = [i["title"] for i in seven_day.select(".tombstone-container img")]
+    temps = [i.get_text() for i in seven_day.select(".tombstone-container .temp")]
+
+    
+    
+    
+    weather = pd.DataFrame({
+        "Period:": periods,
+        "Description:": descs,
+        "Temperature:": temps,
+    })
+    
+    print("\n",weather)
+
+    
+    
 
 
 
 def main():
-    print_test()
-    scraping_classes_and_ids()
+    test_pages = False
+    main_page = True
 
-main()
+    if test_pages:
+        print_test()
+        scraping_classes_and_ids()
+    
+
+    if main_page:
+        scraping_main_page()
+
+
+if __name__ == "__main__":
+    main()
